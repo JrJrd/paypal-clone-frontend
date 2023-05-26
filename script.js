3c21dab0da9494c1d8e6a674f3063f175b7fa05a
JJ <jrd.seaer@gmail.com>
2023-05-26 14:21:58 +0300

    JS Dropdown NAV
    Nav dropdown menu

---
+++ script.js
@@ -0,0 +1,13 @@
+const dropdownItems = document.querySelectorAll('.dropdown-hover')
+dropdownItems.forEach(dropdownItem => {
+    dropdownItem.addEventListener('mouseover', () => {
+        dropdownItem.lastElementChild.style.cssText = 'opacity: 1; visibility: visible'
+        document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+        dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(180deg)'
+    })
+    dropdownItem.addEventListener('mouseout', () => {
+        dropdownItem.lastElementChild.style.cssText = 'opacity: 0; visibility: hidden'
+        document.querySelector('.navbar-wrapper').style.background = 'none'
+        dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(0)'
+    })
+})
